4ab3e5e788e95aa5cf8bbf75b835f4c4a18433cb
Somya Shrestha <90999893+shalshcode08@users.noreply.github.com>
2024-10-04 20:19:04 +0530

    input box component added

--- currency-converter/src/App.jsx
+++ currency-converter/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import InputBox from './components/InputBox';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='text-3xl'>
-      Hello World
+      <InputBox/>
     </div>
   )
 }
