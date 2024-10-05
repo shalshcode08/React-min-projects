ce9aab6155c9f007ab8cd4e47b46c6eb6e8d1bc3
Somya Shrestha <90999893+shalshcode08@users.noreply.github.com>
2024-10-05 15:16:34 +0530

    complete

--- minicontext-project/src/App.jsx
+++ minicontext-project/src/App.jsx
@@ -1,5 +1,3 @@
-
-import './App.css'
 import Login from './components/Login'
 import Profile from './components/Profile'
 import UserContextProvider from './context/UserContextProvider'
@@ -9,7 +7,7 @@ function App() {
 
   return (
     <UserContextProvider>
-      <h1>React with Chai and share is important</h1>
+      <h1>Hello</h1>
       <Login />
       <Profile />
     </UserContextProvider>
